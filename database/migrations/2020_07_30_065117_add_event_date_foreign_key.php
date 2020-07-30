6494c93b3570a790344a9a03ff0c8d8f98ef8ef8
Paul Phillip Villarosa <phillip.villarosa@gmail.com>
2020-07-30 15:13:05 +0800

    added database

---
+++ database/migrations/2020_07_30_065117_add_event_date_foreign_key.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEventDateForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('event_dates', function (Blueprint $table) {
+            $table->foreignId('event_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event_dates', function (Blueprint $table) {
+            $table->dropForeign(['event_id']);
+        });
+    }
+}
